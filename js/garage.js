4d97383ca08ef595b5f67ffeb49f9372cf733f97
hoanganh15092006 <chaxienhanoi@gmail.com>
2025-11-07 21:16:42 +0700

    no fixxing

--- js/garage.js
+++ js/garage.js
@@ -274,3 +274,16 @@ function SearchIntable() {
     table.style.display = "none";
   }
 }
+// const body = document.body;
+// const switchModeButton = document.getElementById("switchModeBtn");
+// const mode = document.querySelector(".mode-name");
+// const handleChangeMode = () => {
+//   if (switchModeButton.checked) {
+//     body.classList.add("dark-mode");
+//     mode.innerHTML("dark mode");
+//   } else {
+//     body.classList.remove("dark-mode");
+//     mode.innerHTML = "light mode";
+//   }
+// };
+// switchModeButton.addEventListener("change", handleChangeMode);
