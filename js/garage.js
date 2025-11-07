0f9175647c514f1ac06e217bbea89e11b39bade6
hoanganh15092006 <chaxienhanoi@gmail.com>
2025-11-07 19:16:20 +0700

    chart data pakage

--- js/garage.js
+++ js/garage.js
@@ -8,15 +8,28 @@ gradientIN.addColorStop(1, "rgba(219, 163, 98, 0.02)");
 const gradientOUT = ctx.createLinearGradient(0, 0, 0, 400);
 gradientOUT.addColorStop(0, "rgba( 182, 211, 250, 0.1)");
 gradientOUT.addColorStop(1, "rgba( 182, 211, 250, 0.02)");
-
+let timelist = [
+  "0h",
+  "7h",
+  "9h",
+  "11h",
+  "13h",
+  "15h",
+  "17h",
+  "19h",
+  "21h",
+  "22h",
+];
+let chardataxera = [0, 0, 200, 600, 700, 0, 200, 1000, 200, 0];
+let chardataxevao = [200, 800, 1000, 1500, 2000, 1500, 600, 200, 0];
 const myChart = new Chart(ctx, {
   type: "line",
   data: {
-    labels: ["0h", "7h", "9h", "11h", "13h", "15h", "17h", "19h", "21h", "22h"],
+    labels: timelist,
     datasets: [
       {
         label: "Luợng xe vào",
-        data: [0, 200, 800, 1000, 1500, 2000, 1500, 600, 200, 0],
+        data: chardataxevao,
         borderColor: "rgba( 219, 163, 98, 1)",
         backgroundColor: gradientIN,
         tension: 0.4,
@@ -29,7 +42,7 @@ const myChart = new Chart(ctx, {
 
       {
         label: "Luợng xe ra",
-        data: [0, 0, 200, 600, 700, 0, 200, 1000, 200, 0],
+        data: chardataxera,
         borderColor: "rgba( 182, 211, 250, 1)",
         backgroundColor: gradientOUT,
         fill: true,
